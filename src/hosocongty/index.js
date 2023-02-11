268d25c1cbfe22c6e5df2bd96f8e73206ca83280
GiangND <giang21199z@gmail.com>
2023-02-11 08:17:21 +0700

    update insert bulk index

--- src/hosocongty/index.js
+++ src/hosocongty/index.js
@@ -13,27 +13,28 @@ const totalPage = 226667;
 const size = 12;
 const execute = async () => {
   for (let page = 1; page <= totalPage; page++) {
-    let count = 1;
     try {
       const url = `https://hosocongty.vn/page-${page}`;
       const response = await request(url);
       console.log(`Get page ${page} success`);
       const $ = cheerio.load(response);
+      const arr = [];
       $('.hsdn li').each((index, li) => {
         const aTag = $(li).find('h3 a');
         if (aTag && $(aTag).attr('href')) {
           const name = $(aTag).text();
           const link = $(aTag).attr('href');
           const source = 2;
-          const companyLink = { name, link, source };
-          const query = connection.query('INSERT INTO company_link SET ?', companyLink, function (error, results, fields) {
-            if (error) {
-              console.log(`Error insert company:`, JSON.stringify(companyLink));
-            }
-            console.log(`Inserted ${(page - 1) * size + (count++)} success`);
-          });
+          const companyLink = [name, link, source];
+          arr.push(companyLink);
         }
       })
+      connection.query('INSERT INTO company_link (name, link, source) VALUES ?', [arr], function (error, results, fields) {
+        if (error) {
+          console.log(`Error insert company:`, error);
+        }
+        console.log(`Inserted ${page} success`);
+      });
     } catch (err) {
       console.log(`Error crawler page: ${page}`, err);
     }
